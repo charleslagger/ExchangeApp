b9f16faf5e50cfe926cd4c53b14cdcd354c52397
charleslagger <nvkhoeuet@gmail.com>
2018-11-07 00:55:11 +0700

    update

--- server/build/pre.sh
+++ server/build/pre.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 cd
+echo "Remove previous version server"
 rm -R .composer/
 cd fabric-dev-servers/
+export FABRIC_VERSION=hlfv11
 sudo ./teardownFabric.sh
 echo "Starting Hyperledger composer server"
 sudo ./startFabric.sh
@@ -13,8 +15,8 @@ cd HyperledgerComposers/
 echo "Replace name project"
 cd ExchangeApp/server/exchange
 composer archive create -t dir -n .
-composer network install -a exchange@0.5.1.bna -c PeerAdmin@hlfv1
-composer network start -n exchange -V 0.5.1 -A admin -S adminpw -c PeerAdmin@hlfv1
+composer network install -a exchange@0.0.2.bna -c PeerAdmin@hlfv1
+composer network start -n exchange -V 0.0.2 -A admin -S adminpw -c PeerAdmin@hlfv1
 composer card import -f admin@exchange.card
 composer card list
 
