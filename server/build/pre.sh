387ce687d9ba9ffc2310e7c76b1c3e4cc7a5c246
charleslagger <nvkhoeuet@gmail.com>
2018-11-20 15:41:49 +0700

    replace exchange multiple client app

--- server/build/pre.sh
+++ server/build/pre.sh
@@ -3,7 +3,7 @@ cd
 echo "Remove previous version server"
 rm -R .composer/
 cd fabric-dev-servers/
-export FABRIC_VERSION=hlfv11
+export FABRIC_VERSION=hlfv12
 sudo ./teardownFabric.sh
 echo "Starting Hyperledger composer server"
 sudo ./startFabric.sh
@@ -15,8 +15,8 @@ cd HyperledgerComposers/
 echo "Install and deply card"
 cd ExchangeApp/server/exchange
 composer archive create -t dir -n .
-composer network install -a exchange@0.0.2.bna -c PeerAdmin@hlfv1
-composer network start -n exchange -V 0.0.2 -A admin -S adminpw -c PeerAdmin@hlfv1
+composer network install -a exchange@0.0.1.bna -c PeerAdmin@hlfv1
+composer network start -n exchange -V 0.0.1 -A admin -S adminpw -c PeerAdmin@hlfv1
 composer card import -f admin@exchange.card
 composer card list
 
