578900a665005c60edcd67dcdcf6a23301782693
Khoe <nvkhoeuet@gmail.com>
2018-11-23 10:29:42 +0700

    Update admin-client.sh

--- server/build/admin-client.sh
+++ server/build/admin-client.sh
@@ -4,4 +4,4 @@ cd HyperledgerComposers/
 cd ExchangeApp/server/exchange/
 echo "Starting composer rest server for admin at port 3001"
 export COMPOSER_TLS=true
-composer-rest-server -c admin@exchange -p 443 -n never
+composer-rest-server -c admin@exchange -p 4200 -n never
